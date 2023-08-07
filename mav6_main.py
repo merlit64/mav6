6bcf3323b28c5205f80f3c49a540ed1dbee9e9ff
Paul Merlitti <pmerlitt@cisco.com>
2023-08-07 15:01:57 -0400

    Updated mav6_main.py with Comment lines for different Tests

--- mav6_main.py
+++ mav6_main.py
@@ -19,6 +19,11 @@ def ping_host(ipaddress):
 
 ######## MAIN PROGRAM ########
 
+# Note: ALL comments are made from the perspective of the test device
+# I.E. Telnet server test means the Test device is acting as the TFTP Server
+
+
+### SERVER TESTS ###
 
 # Ping Server Test
 ping_host(TEST_DEVICE)
@@ -32,3 +37,87 @@ print(colored("Testing termcolor", "red"))
 
 
 # SCP Server Test
+
+
+# TFTP Server Test
+
+
+# HTTP Server Test
+
+
+# HTTPS Server Test
+
+
+# SNMP v2 Read Test
+
+
+# SNMP v2 Write Test
+
+
+# SNMP v3 Read Test
+
+
+# SNMP v3 Write Test
+
+
+# NTP v4 Server Test
+
+
+# DHCP Server Test
+
+
+
+### CLIENT TESTS ###
+
+# Ping Client Test
+ping_host(TEST_DEVICE)
+print(colored("Testing termcolor", "red"))
+
+
+# Telnet Client Test
+
+
+# SSH Client Test
+
+
+# DNS Client Test
+
+
+# SCP client Test
+
+
+# TFTP client Test
+
+
+# HTTP client Test
+
+
+# HTTPS client Test
+
+
+# SNMP v2 Trap Test
+
+
+# SNMP v3 Trap Test
+
+
+# NTP v4 Client Test
+
+
+# DHCP Client Test
+
+
+# Syslog Client Test
+
+
+# Streaming Telemetry Test
+
+
+# Netflow Tests
+
+
+# TACACS+ Test
+
+
+# RADIUS Test
+
