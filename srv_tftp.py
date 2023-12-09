2473fe329adf42585c01e3a30fc41cb43cd0e5d2
Paul Merlitti <pmerlitt@cisco.com>
2023-12-09 15:04:02 -0500

    added file_on_mav, del_from_mav functions

--- srv_tftp.py
+++
@@ -1,7 +0,0 @@
-from tftpy import TftpServer
-from time import sleep
-import socket
-
-srv1 = TftpServer('..')
-srv1.listen(listenip='10.112.1.106', listenport=69, af_family=socket.AF_INET)
-sleep(150)
