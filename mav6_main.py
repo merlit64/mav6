a824bf1443ba7c479fadbfb93ec8bbca987cd9a1
Paul Merlitti <pmerlitt@cisco.com>
2023-12-14 12:55:13 -0500

    Created a function to build CA and sign a server cert

--- mav6_main.py
+++ mav6_main.py
@@ -488,7 +488,22 @@ def filetransfer_client_download(device_hostname='', device_protocol='ssh',
         print("File transfer protocol not supported.")
         exit()
 
- 
+
+def ca_create_directory():
+    # Delete old CA
+    if (os.path.isdir('mav6-certs')):
+        shutil.rmtree('mav6-certs')
+
+    # Create the rootCA.key and rootCA.crt
+    os.mkdir('mav6-certs')
+    
+
+def ca_buildca_cert():
+    os.chdir('mav6-certs')
+    command = 'openssl req -x509 -sha256 -days 3650 -nodes  -newkey rsa:4096 -subj ' + \
+                '"/CN=mav6b.ciscofederal.com/C=US/L=Richfield/ST=Ohio"  -keyout rootCA.key -out rootCA.crt'
+    os.system(command)
+
 def ca_buildca(server_ip=''):
     # Delete old CA
     if (os.path.isdir('mav6-certs')):
@@ -520,6 +535,76 @@ def ca_buildca(server_ip=''):
     #get fingerprint of rootCA.crt
     command = 'openssl z509 in rootCA.crt -noout -fingerprint >> fingerprint.txt'
     os.system(command)
+
+def ca_create_key_and_cert(directory = '', key_cert_name = '', issuer='self', bits=4096, hash='sha256'):
+    # if issuer = 'self' and key_cert_name = 'rootCA' this function will create the directory and build rootCA key and cert
+    # if the issuer is self and key_cert_name is something other than 'rootCA', we will create a key and a self-signed cert
+    # or else this function will create a cert and key for key_cert_name with rootCA as the issuer
+    PATH = './' + directory
+    PRIV_KEY_FILE = key_cert_name + '.key'
+    PUB_KEY_FILE = key_cert_name + '-pub.key'
+    CERT_FILE = key_cert_name + '.crt'
+    CA_KEY_FILE = issuer + '.key'
+    CA_CERT_FILE = issuer + '.crt'
+
+
+    k = crypto.PKey()
+    k.generate_key(crypto.TYPE_RSA, bits)
+
+    cert = crypto.X509()
+
+    cert.get_subject().C = 'US'
+    cert.get_subject().ST = 'Ohio'
+    cert.get_subject().L = 'Richfield'
+    cert.get_subject().O = 'Cisco'
+    cert.get_subject().OU = 'Federal'
+    cert.get_subject().CN = 'mav6.ciscofederal.com'
+    cert.get_subject().emailAddress = 'pmerlitt@cisco.com'
+
+    cert.set_serial_number(1000)
+    cert.gmtime_adj_notBefore(0)
+    cert.gmtime_adj_notAfter(5 * 365 * 24 * 60 * 60)
+
+    # Issuer could be self or a CA
+    if(issuer == 'self'):
+        cert.set_issuer(cert.get_subject())
+        cert.set_pubkey(k)
+        # Signed with self key or CA key
+        cert.sign(k, hash)
+    else:
+        ca_cert = crypto.load_certificate(crypto.FILETYPE_PEM, 
+                                          open(os.path.join(PATH, CA_CERT_FILE)).read())
+
+        ca_key = crypto.load_privatekey(crypto.FILETYPE_PEM, 
+                                        open(os.path.join(PATH, CA_KEY_FILE)).read())
+
+        cert.set_issuer(ca_cert.get_subject())
+        cert.set_pubkey(k)
+        # Signed with self key or CA key
+        cert.sign(ca_key, hash)
+
+
+    cert_fq_filename = os.path.join(PATH, CERT_FILE)
+    priv_key_fq_filename = os.path.join(PATH, PRIV_KEY_FILE)
+    pub_key_fq_filename = os.path.join(PATH, PUB_KEY_FILE)
+
+    if os.path.exists(directory) and issuer=='self' and key_cert_name=='rootCA':
+        shutil.rmtree(directory)
+        sleep(2)
+        os.mkdir(directory)
+    elif not os.path.exists(directory):
+        os.mkdir(directory)
+    else: 
+        pass    
+    with open(cert_fq_filename, 'wb') as f:
+        f.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
+
+    with open(priv_key_fq_filename, 'wb') as f:
+        f.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, k))
+
+    with open(pub_key_fq_filename, 'wb') as f:
+        f.write(crypto.dump_publickey(crypto.FILETYPE_PEM, k))
+
 
 def rtr_add_trustpoint(device='', fingerprint=''):
     with open('fingerprint.txt') as fileptr:
@@ -782,6 +867,12 @@ if HTTPS_CLIENT:
     
     # USE OS COMMANDS TO CREATE DIR, OPENSSL ROOTCA.KEY ROOTCA.CRT, SERVER.KEY, SERVER.CSR
     # SERVER.CRT, 
+    # Create CA on Mav6 and create a signed cert for Mav6 https server
+    ca_create_key_and_cert(directory=CA_DIRECTORY, key_cert_name=CA_CERT_NAME, issuer='self')
+    ca_create_key_and_cert(directory=CA_DIRECTORY, key_cert_name='server', issuer=CA_CERT_NAME)
+
+
+    # Start Server
     print('starting https server process')
     if (ip_version(TEST_DEVICE) == 4):
         https_server_process = Process(target=start_server, name='httpsserver', 
