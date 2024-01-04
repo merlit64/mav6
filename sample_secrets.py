cad2987f8444f385ff05463eac33697b16a0d62e
Paul Merlitti <pmerlitt@cisco.com>
2024-01-04 09:56:49 -0500

    import secrets instead of secrets_1 and add sample

---
+++ sample_secrets.py
@@ -0,0 +1,54 @@
+######## MACROS ########
+
+######## HERNDON PASSWORDS ########
+#TEST_DEVICE = "***REMOVED***"
+#COM_RO = "***REMOVED***"
+#COM_RW = "***REMOVED***"
+#AUTH_KEY = "***REMOVED***"
+#PRIV_KEY = "***REMOVED***"
+#SNMP_USER = "***REMOVED***"
+#CLI_USER = "***REMOVED***"
+#USER_PASS = "***REMOVED***"
+
+
+
+####### RICHFIELD PASSWORDS ######
+TEST_DEVICE = "10.112.1.61"
+#COM_RO = "***REMOVED***"
+#COM_RW = "***REMOVED***"
+#AUTH_KEY = "***REMOVED***"
+#PRIV_KEY = "***REMOVED***"
+#SNMP_USER = "***REMOVED***"
+#CLI_USER = "***REMOVED***"
+#USER_PASS = "***REMOVED***"
+
+
+####### Test CSR100v ######
+#TEST_DEVICE = "10.112.1.201"
+#TEST_DEVICE = "2005:1117:1:1::0A70:01C9"
+#COM_RO = "***REMOVED***"
+#COM_RW = "***REMOVED***"
+#AUTH_KEY = "***REMOVED***"
+#PRIV_KEY = "***REMOVED***"
+#SNMP_USER = "***REMOVED***"
+#CLI_USER = "***REMOVED***"
+#USER_PASS = "***REMOVED***"
+
+
+####### Mgmt Switch ######
+TEST_DEVICE = "10.1.1.1"
+TEST_DEVICE = "2001:db8::1"
+TEST_DEVICE_HOSTNAME = 'test_device'
+AUTH_KEY = "some_password"
+PRIV_KEY = "some_password"
+SNMP_USER = "v3_username"
+COM_RO = "snmp_ro"
+COM_RW = "snmp_rw"
+CLI_USER = "cli_username"
+USER_PASS = "cli_password"
+
+####### MAV6 Server INFO #######
+# IPv4 and IPv6  address of the Mav6 server
+# Note: First release only supports Ubuntu
+MAV6_IPV4 = "10.112.1.106"
+MAV6_IPV6 = "2005:1117:1:1:3325:e920:cf17:bcee"
